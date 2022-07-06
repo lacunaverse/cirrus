f7ac02886e66f1f6351f0bd34345926a1bfae327
hvlck <just.ethan@protonmail.com>
2022-07-06 18:47:50 -0400

    chore: tests

---
+++ main_test.go
@@ -0,0 +1,29 @@
+package synapse
+
+import "testing"
+
+func TestURL(t *testing.T) {
+	v, err := Determine("https://github.com/")
+	if err != nil {
+		t.Fail()
+	}
+
+	if v.Value != "https://github.com/" && v.ResultType != LINK {
+		t.Fail()
+	}
+}
+
+func TestHasUnit(t *testing.T) {
+	v, exists := hasUnit("length of 10m")
+
+	if !exists || v != METERS {
+		t.Fail()
+	}
+}
+
+func TestExtractUnit(t *testing.T) {
+	un, value := extractUnit("length of 10m")
+	if un != METERS && value != "10" {
+		t.Fail()
+	}
+}
